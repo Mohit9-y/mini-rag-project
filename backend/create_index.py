516a04a45cc6a97f8ae9aad4871d662753cb2c29
Mohit Yadav <mohitpsf@gmail.com>
2026-03-24 12:44:35 +0530

    add data folder to store given files

---
+++ backend/create_index.py
@@ -0,0 +1,8 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+import os
+
+DATA_path = "../data/raw/"
+INDEX_PATH = "../faiss_index/"
